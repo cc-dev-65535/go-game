9111d952aa20581cc8241495b63c4358a6281952
Collin <collinc812@gmail.com>
2024-12-16 14:32:54 -0800

    Add tiling

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"image"
 	_ "image/png"
 	"log"
+	"os"
 
 	"game/test/images"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/lafriks/go-tiled"
+	"github.com/lafriks/go-tiled/render"
 )
 
 type Game struct{ layers [][]int }
@@ -36,9 +39,7 @@ func (g *Game) Update() error {
 
 func getImageFromSpritesheet(imageFile *ebiten.Image, cell int) *ebiten.Image {
 	width := imageFile.Bounds().Dx()
-	fmt.Println(width)
 	tileXCount := width / tileSize
-	fmt.Println(tileXCount)
 
 	sx := (cell % tileXCount) * tileSize
 	sy := (cell / tileXCount) * tileSize
@@ -51,15 +52,54 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(0, 0)
 
-	const xCount = 320 / tileSize
-	for _, layer := range g.layers {
-		for i, cell := range layer {
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64((i%xCount)*tileSize), float64((i/xCount)*tileSize))
-
-			screen.DrawImage(getImageFromSpritesheet(tilesImage, cell), op)
-		}
+	gameMap, err := tiled.LoadFile("maps/game.tmx")
+	if err != nil {
+		fmt.Printf("error parsing map: %s", err.Error())
+		panic(err)
 	}
+
+	fmt.Println(gameMap)
+
+	// You can also render the map to an in-memory image for direct
+	// use with the default Renderer, or by making your own.
+	renderer, err := render.NewRenderer(gameMap)
+	if err != nil {
+		fmt.Printf("map unsupported for rendering: %s", err.Error())
+		os.Exit(2)
+	}
+
+	// Render just layer 0 to the Renderer.
+	err = renderer.RenderLayer(0)
+	if err != nil {
+		fmt.Printf("layer unsupported for rendering: %s", err.Error())
+		os.Exit(2)
+	}
+
+	// Get a reference to the Renderer's output, an image.NRGBA struct.
+	img := renderer.Result
+	var testImage image.Image = img
+
+	// make image
+	// ebiten.NewImageFromImage(testImage)
+
+	// Clear the render result after copying the output if separation of
+	// layers is desired.
+	renderer.Clear()
+
+	// And so on. You can also export the image to a file by using the
+	// Renderer's Save functions.
+
+	screen.DrawImage(ebiten.NewImageFromImage(testImage), op)
+
+	// const xCount = 320 / tileSize
+	// for _, layer := range g.layers {
+	// 	for i, cell := range layer {
+	// 		op := &ebiten.DrawImageOptions{}
+	// 		op.GeoM.Translate(float64((i%xCount)*tileSize), float64((i/xCount)*tileSize))
+
+	// 		screen.DrawImage(getImageFromSpritesheet(tilesImage, cell), op)
+	// 	}
+	// }
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -67,7 +107,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	game := &Game{layers: layers}
+	game := &Game{}
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("My Game")
 	if err := ebiten.RunGame(game); err != nil {
